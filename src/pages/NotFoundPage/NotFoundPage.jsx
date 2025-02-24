af0b478d3326b9c6bb136a7a0ab9a35b4d5ed7c1
wjddydwns <wjddydwns159@gmail.com>
2025-02-24 11:44:28 +0900

    add+ not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Aurora from '../../assets/Backgrounds/Aurora/Aurora'
+import './NotFoundTitle.css'
 
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div className='error_page'>
+      <Aurora
+      colorStops={["#8F002B", "#8F002B", "#8F002B"]}
+      />
+      <div className='error_msg'>404<br/>이런! 요청하신 페이지가 없습니다.</div>
+    </div>
   )
 }
 
